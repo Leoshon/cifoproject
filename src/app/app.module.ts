faa29fb88e1124888b5657d754399496edb9d8ac
Leoshon <131702139+Leoshon@users.noreply.github.com>
2024-01-30 06:28:23 +0100

    deploy configs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -11,11 +11,17 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule,HttpClientModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp({"projectId":"didacticfire","appId":"1:89947718031:web:7d77235f0cef0f29f18bfa","storageBucket":"didacticfire.appspot.com","apiKey":"AIzaSyDDp2DQmMtQ6ZujfAG2wjTnStHlTMyGTLA","authDomain":"didacticfire.firebaseapp.com","messagingSenderId":"89947718031"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())],
+  imports: [BrowserModule,HttpClientModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp({"projectId":"didacticfire","appId":"1:89947718031:web:7d77235f0cef0f29f18bfa","storageBucket":"didacticfire.appspot.com","apiKey":"AIzaSyDDp2DQmMtQ6ZujfAG2wjTnStHlTMyGTLA","authDomain":"didacticfire.firebaseapp.com","messagingSenderId":"89947718031"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage()), ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
